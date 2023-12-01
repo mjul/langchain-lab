bf17e845fb702538e7f159cb892e4a3ca4287c69
Martin Jul <martin@mjul.com>
2023-12-01 20:35:18 +0100

    Add a bit more documentation

--- langserve_demo.py
+++ langserve_demo.py
@@ -3,7 +3,7 @@
 # Remember to set the OPENAI_API_KEY environment variable to an OpenAI API key.
 #
 # You can try the joke generator by running this file and then visiting http://localhost:8000/joke/playground/
-#
+# The server API documentation is available at http://localhost:8000/docs
 from fastapi import FastAPI
 from langchain.chat_models import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate
@@ -12,7 +12,7 @@ from langserve import add_routes
 app = FastAPI(
     title="LangChain Server",
     version="1.0",
-    description="A simple API server using Langchain's Runnable interfaces",
+    description="A simple API server using LangChain's Runnable interfaces",
 )
 
 add_routes(
